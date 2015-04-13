25a94f6ff3a8c7cff141a6c7d58c8a8986109440
Davesmall28 <dave@dsmall.co.uk>
2015-04-13 20:14:01 +0100

    Setup VS Solution  and added files.

---
+++ DSmall.DynamicsCrm.Core/NuGetPush.bat
@@ -0,0 +1,3 @@
+..\..\NuGet\NuGet.exe pack ../DSmall.DynamicsCrm.Core.csproj -Build -symbols
+
+pause
---
+++ DSmall.DynamicsCrm.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DSmall.DynamicsCrm.Core")]
+[assembly: AssemblyDescription("The core Dynamics Crm package from DSmall Solutions Ltd.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DSmall Solutions Ltd")]
+[assembly: AssemblyProduct("DSmall.DynamicsCrm.Core")]
+[assembly: AssemblyCopyright("Copyright © DSmall Solutions Ltd 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("105d99ea-5a38-4b8c-b3fb-4a89a33683ce")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
